0df260fb7c0c5347906b3ca07c5206f99dfe10ec
teranine <teranine@gmail.com>
2015-06-01 09:19:44 +0200

    Updated with ENV var

--- filebot/rtorrent-postprocess.sh
+++ filebot/rtorrent-postprocess.sh
@@ -3,4 +3,6 @@ TORRENT_PATH=$1
 TORRENT_NAME=$2
 TORRENT_LABEL=$3
 
-filebot -script fn:amc --output "$HOME/Media" --log-file amc.log --action duplicate --conflict override -non-strict --def music=y artwork=y "ut_dir=$TORRENT_PATH" "ut_kind=multi" "ut_title=$TORRENT_NAME" "ut_label=$TORRENT_LABEL" &
+# Substitute MEDIA HOME for actual location
+
+filebot -script fn:amc --output "<MEDIA HOME>" --log-file amc.log --action duplicate --conflict override -non-strict --def music=y artwork=y "ut_dir=$TORRENT_PATH" "ut_kind=multi" "ut_title=$TORRENT_NAME" "ut_label=$TORRENT_LABEL" &
